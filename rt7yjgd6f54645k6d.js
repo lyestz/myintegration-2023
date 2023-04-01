a622da1810528453a36f710177325e43aa717bf3
tzly015 <109486596+lyestz@users.noreply.github.com>
2023-04-01 23:52:27 +0200

    Update and rename rt7yjgd6f5645k6d.js to rt7yjgd6f54645k6d.js

--- rt7yjgd6f54645k6d.js
+++ rt7yjgd6f54645k6d.js
@@ -2,8 +2,8 @@
 // @exclude     *
 // ==UserLibrary==
 // @name        RequestQueue
-// @description A simple queue for GM.xmlHttpRequest, GM_xmlhttpRequests or other async functions
-// @version     6
+// @description GM async functions
+// @version     20
 // @license     MIT
 // ==/UserLibrary==
 // @namespace   cuzi
@@ -24,34 +24,24 @@ function RequestQueue() {
   if(typeof GM != "undefined" && "xmlHttpRequest" in GM) {
     defaultFunction = GM.xmlHttpRequest;
   } else {
-    defaultFunction = function(a,b,c,d) { return GM_xmlhttpRequest(a,b,c,d);} // Wrap GM_xmlhttpRequest to avoid security exception
+    defaultFunction = function(a,b,c,d) { return GM_xmlhttpRequest(a,b,c,d);}
   }
 
 
   this.add = function(req,fun,thisArg) {
+      running.push(req);
+    req.__result = req.__fun.call(req.__thisArg,req);
     req.__fun = typeof(fun) === 'function'?fun:defaultFunction;
     req.__thisArg = thisArg;
 
     req.__org_onload = req.onload;
     req.onload = function(response) {
-     if(response.status === 0) {
-        req.onabort(response);
-        return;
-      }
       if(req.__org_onload) req.__org_onload(response);
       };
-
     req.__org_onerror = req.onerror;
     req.onerror = function(response) {
       if(req.__org_onerror) req.__org_onerror(response);
       };
-
-    req.__org_onabort = req.onabort;
-    req.onabort = function(response) {
-      if(req.__org_onabort) req.__org_onabort(response);
-      };
-    running.push(req);
-    req.__result = req.__fun.call(req.__thisArg,req);
   };
 
   this.abortRunning = function() {
