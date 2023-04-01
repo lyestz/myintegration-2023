c55935390b5e7cb811f09c8c6be2d24bc22eb54b
tzly015 <109486596+lyestz@users.noreply.github.com>
2023-04-01 03:07:13 +0200

    Update and rename spsp12358569.js to spsp12358569tz.js

--- spsp12358569tz.js
+++ spsp12358569tz.js
@@ -16,51 +16,23 @@
 
 "use strict";
 
-function RequestQueue(maxParallel,maxTotal) {
-  // A simple queue for GM_xmlhttpRequests or other async functions
-
-  maxParallel = parseInt(maxParallel)>0?parseInt(maxParallel):1;
+function RequestQueue(maxTotal) {
   maxTotal = parseInt(maxTotal)>0?parseInt(maxTotal):Number.POSITIVE_INFINITY;
 
   var index = 0; // incrementing, used for unique ids
   var finished = 0; // Number of finished requests
   var pending = [];
   var running = [];
-  
-  
-  
-  
+
   var defaultFunction;
   if(typeof GM != "undefined" && "xmlHttpRequest" in GM) {
     defaultFunction = GM.xmlHttpRequest;
   } else {
-    defaultFunction = function(a,b,c,d) { return GM_xmlhttpRequest(a,b,c,d);} // Wrap GM_xmlhttpRequest to avoid security exception
+    defaultFunction = function(a,b,c,d) { return GM_xmlhttpRequest(a,b,c,d);}
   }
-
-
-  /*
-  The internal request Object extended by add(req,fun,thisArg)
-  {
-    __fun : fun, defaults to GM_xmlhttpRequest
-    __thisArg : thisArg, the this object for the above function
-    __result : the return value of __fun is saved in this property
-
-    __org_onload : The original event callbacks of req
-    __org_onerror
-    __org_onabort
-
-    onload : The new event callbacks that wrap the above callbacks
-    onerror
-    onabort
-
-    ... and all the original properties of the "req" object
-  }
-
-  */
 
   var fire = function() {
-    // Check for pending requests and send them.
-    if(pending.length > 0 && running.length < maxParallel && finished < maxTotal) {
+    if(pending.length > 0 && finished < maxTotal) {
       var req = pending.shift();
       running.push(req);
       req.__result = req.__fun.call(req.__thisArg,req);
@@ -69,7 +41,6 @@ function RequestQueue(maxParallel,maxTotal) {
   };
 
   var remove = function(id) {
-    // Remove the request with id from the the running array
     for(var i = 0; i < running.length; i++) {
       if(running[i].id == id) {
         running.splice(i, 1);
@@ -81,18 +52,14 @@ function RequestQueue(maxParallel,maxTotal) {
   };
 
   this.add = function(req,fun,thisArg) {
-    // Schedule a request: add(req[, fun[, thisArg]])
-    // fun:      defaults to GM.xmlHttpRequest
-    // thisArg:  The value of this provided for the call to fun. Keep strict mode in mind!
     req.id = index++;
 
-    req.__fun = typeof(fun) === 'function'?fun:defaultFunction; 
+    req.__fun = typeof(fun) === 'function'?fun:defaultFunction;
     req.__thisArg = thisArg;
 
-    // Wrap events that indicate that the request has finished
     req.__org_onload = req.onload;
     req.onload = function(response) {
-     if(response.status === 0) { // This is true if a request was aborted. This seems to happen only sporadically
+     if(response.status === 0) {
         req.onabort(response);
         return;
       }
@@ -117,9 +84,6 @@ function RequestQueue(maxParallel,maxTotal) {
   };
 
   this.abortRunning = function() {
-    // For any added function that was already run: result = thisArg.fun(req)
-    // it will call result.abort()
-    // For GM_xmlhttpRequest this will subsequently fire an onabort event
     for(var i = 0; i < running.length; i++) {
       if(running[i] && typeof running[i].__result.abort === 'function') {
         running[i].__result.abort();
@@ -128,30 +92,20 @@ function RequestQueue(maxParallel,maxTotal) {
   };
 
   this.abortPending = function() {
-    // Clear the pending list
     pending = [];
   };
 
   this.abort = function() {
-    // Abort both running and pending requests
     this.abortPending();
     this.abortRunning();
   };
-  
   this.resetTotal = function() {
-    // Reset number of finished requests
     finished = 0;
   }
-  
   this.hasReachedTotal = function() {
-    // Number of maximum allowed requests reached?
     return finished >= maxTotal;
   }
-  
   this.hasRunning = function() {
-    // Are there any running requests?
     return running.length > 0;
   }
-  
-  
 }
