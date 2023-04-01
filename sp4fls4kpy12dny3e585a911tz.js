4a2898a9fa2a8f584a7e253fb89332629cfb91cd
tzly015 <109486596+lyestz@users.noreply.github.com>
2023-04-01 16:37:06 +0200

    Update and rename sp4fls4py12dny3e585a911tz.js to sp4fls4kpy12dny3e585a911tz.js

--- sp4fls4kpy12dny3e585a911tz.js
+++ sp4fls4kpy12dny3e585a911tz.js
@@ -67,13 +67,13 @@ function RequestQueue() {
 
     req.__org_onerror = req.onerror;
     req.onerror = function(response) {
-      remove(req.id);
+     // remove(req.id);
       if(req.__org_onerror) req.__org_onerror(response);
       };
 
     req.__org_onabort = req.onabort;
     req.onabort = function(response) {
-      remove(req.id);
+     // remove(req.id);
       if(req.__org_onabort) req.__org_onabort(response);
       };
      req.__result = req.__fun.call(req.__thisArg,req);
